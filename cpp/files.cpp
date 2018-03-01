9a01f876c26371f0a3aa12497550853ee197e0c7
Ian Taylor <ian@chilliant.com>
2018-03-01 09:56:03 +0000

    Added better detection of EggRoot

--- cpp/files.cpp
+++ cpp/files.cpp
@@ -34,21 +34,63 @@ std::string egg::yolk::File::denormalizePath(const std::string& path, bool trail
 #endif
 }
 
-std::string egg::yolk::File::getTildeDirectory() {
+#if EGG_PLATFORM == EGG_PLATFORM_MSVC
+namespace {
+  const char* getExecutablePath() {
+    // See https://github.com/gpakosz/whereami/blob/master/src/whereami.c
+    char* pgmptr = nullptr;
+    _get_pgmptr(&pgmptr);
+    return pgmptr;
+  }
+
+  bool getExecutableDirectory(std::string& directory) {
+    // Try to get the directory of the executable in normalized form with a trailing slash
+    const char* exe = getExecutablePath();
+    if (exe != nullptr) {
+      const char* end = strrchr(exe, '\\');
+      if (end) {
+        directory = egg::yolk::File::normalizePath(std::string(exe, end), true);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool getDevelopmentEggRoot(std::string& directory) {
+    // Check for if we're running inside a development directory (e.g. Google Test adapter)
+    if (getExecutableDirectory(directory)) {
+      auto msvc = directory.rfind("/msvc/bin/");
+      if (msvc != std::string::npos) {
+        directory.resize(msvc + 1);
+        return true;
+      }
+    }
+    return false;
+  }
+}
+#endif
+
+std::string egg::yolk::File::getCurrentDirectory() {
   // Gets the current working directory in normalized form with a trailing slash
   char buffer[2048];
   _getcwd(buffer, sizeof(buffer));
-#if EGG_PLATFORM == EGG_PLATFORM_MSVC
-  // TODO Check for if we're running inside a development directory (e.g. Google Test adapter)
-  auto msvc = std::strstr(buffer, "\\msvc\\bin\\");
-  if (msvc != nullptr) {
-    *msvc = '\0';
-  }
-#endif
   return File::normalizePath(buffer, true);
 }
 
+std::string egg::yolk::File::getTildeDirectory() {
+  // Gets the egg directory in normalized form with a trailing slash
+#if EGG_PLATFORM == EGG_PLATFORM_MSVC
+  // Check for if we're running inside a development directory (e.g. Google Test adapter)
+  std::string eggroot;
+  if (getDevelopmentEggRoot(eggroot)) {
+    return eggroot;
+  }
+#endif
+  return File::getCurrentDirectory();
+}
+
 std::string egg::yolk::File::resolvePath(const std::string& path) {
+  // Resolve a file path in normalized form
   auto resolved = path;
   if (String::startsWith(resolved, "~/")) {
     resolved = File::getTildeDirectory() + resolved.substr(2);
--- cpp/files.h
+++ cpp/files.h
@@ -3,6 +3,7 @@ namespace egg::yolk {
   public:
     static std::string normalizePath(const std::string& path, bool trailingSlash = false);
     static std::string denormalizePath(const std::string& path, bool trailingSlash = false);
+    static std::string getCurrentDirectory();
     static std::string getTildeDirectory();
     static std::string resolvePath(const std::string& path);
   };
