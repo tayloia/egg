f7864b9404f7cf8d99cd889190bd6a9eb3195e22
Ian Taylor <ian@chilliant.com>
2018-08-02 10:56:18 +0100

    Added better allocator testing

--- ovum/test.h
+++ ovum/test.h
@@ -1,2 +1,37 @@
 #include "ovum/ovum.h"
 #include "ovum/test/gtest.h"
+
+namespace egg::test {
+  class Allocator final : public egg::ovum::AllocatorDefault {
+    Allocator(const Allocator&) = delete;
+    Allocator& operator=(const Allocator&) = delete;
+  public:
+    enum class Expectation {
+      NoAllocations,
+      AtLeastOneAllocation
+    };
+  private:
+    Expectation expectation;
+  public:
+    explicit Allocator(Expectation expectation = Expectation::AtLeastOneAllocation) : expectation(expectation) {}
+    ~Allocator() {
+      this->validate();
+    }
+    void validate() const {
+      egg::ovum::IAllocator::Statistics stats;
+      ASSERT_TRUE(this->statistics(stats));
+      ASSERT_EQ(stats.currentBlocksAllocated, 0u);
+      ASSERT_EQ(stats.currentBytesAllocated, 0u);
+      switch (this->expectation) {
+      case Expectation::NoAllocations:
+        ASSERT_EQ(stats.totalBlocksAllocated, 0u);
+        ASSERT_EQ(stats.totalBytesAllocated, 0u);
+        break;
+      case Expectation::AtLeastOneAllocation:
+        ASSERT_GT(stats.totalBlocksAllocated, 0u);
+        ASSERT_GT(stats.totalBytesAllocated, 0u);
+        break;
+      }
+    }
+  };
+}
--- ovum/test/string.cpp
+++ ovum/test/string.cpp
@@ -10,38 +10,23 @@ namespace {
       this->end = this->begin + strlen(text);
     }
   };
-
-  void allocatorStatisticsTest(egg::ovum::IAllocator& allocator) {
-    egg::ovum::IAllocator::Statistics statistics;
-    ASSERT_TRUE(allocator.statistics(statistics));
-    ASSERT_EQ(statistics.currentBlocksAllocated, 0u);
-    ASSERT_EQ(statistics.currentBytesAllocated, 0u);
-    ASSERT_GT(statistics.totalBlocksAllocated, 0u);
-    ASSERT_GT(statistics.totalBytesAllocated, 0u);
-  }
 }
 
 TEST(TestString, StringCreateBytes) {
-  egg::ovum::AllocatorDefault allocator;
-  {
-    Literal hello("hello world");
-    auto str = egg::ovum::StringFactory::fromUTF8(allocator, hello.begin, hello.end);
-    ASSERT_EQ(11u, str->length());
-    auto memory = str->memoryUTF8();
-    ASSERT_NE(nullptr, memory);
-    ASSERT_EQ(11u, memory->bytes());
-  }
-  allocatorStatisticsTest(allocator);
+  egg::test::Allocator allocator;
+  Literal hello("hello world");
+  auto str = egg::ovum::StringFactory::fromUTF8(allocator, hello.begin, hello.end);
+  ASSERT_EQ(11u, str->length());
+  auto memory = str->memoryUTF8();
+  ASSERT_NE(nullptr, memory);
+  ASSERT_EQ(11u, memory->bytes());
 }
 
 TEST(TestString, StringCreateBuffer) {
-  egg::ovum::AllocatorDefault allocator;
-  {
-    auto str = egg::ovum::StringFactory::fromUTF8(allocator, u8"egg \U0001F95A");
-    ASSERT_EQ(5u, str->length());
-    auto memory = str->memoryUTF8();
-    ASSERT_NE(nullptr, memory);
-    ASSERT_EQ(8u, memory->bytes());
-  }
-  allocatorStatisticsTest(allocator);
+  egg::test::Allocator allocator;
+  auto str = egg::ovum::StringFactory::fromUTF8(allocator, u8"egg \U0001F95A");
+  ASSERT_EQ(5u, str->length());
+  auto memory = str->memoryUTF8();
+  ASSERT_NE(nullptr, memory);
+  ASSERT_EQ(8u, memory->bytes());
 }
